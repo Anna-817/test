1079386b9fe50175a48268e8b4aef8e620184d60
Anna-817 <81312421+Anna-817@users.noreply.github.com>
2021-03-24 22:31:17 +0300

    first commit

---
+++ static_src/index.jsx
@@ -0,0 +1,43 @@
+import React, {Component} from 'react';
+import ReactDOM from 'react-dom';
+
+class App extends Component {
+   constructor() {
+      super();
+
+      this.state = {
+         messages: ['Привет', 'Как дела?']
+      };
+
+      this.addMessage= () => {
+         this.setState(({messages}) => {
+            return {
+                messages: [...messages, 'Нормально']
+            }
+        });
+   }
+  };
+
+   render (){
+      return (
+         <div>
+            <MessageField messages={ this.state.messages } />
+            <Button addMessage={this.addMessage}/>
+         </div>)
+   }   
+};
+
+const MessageComponent = (props) => <div>{props.text}</div>;
+
+const MessageField = (props) => {
+   return props.messages.map(message => <MessageComponent text={ message } />);
+};
+
+const Button = (props) => {
+   return <a href='#'onClick ={props.addMessage}>link</a>
+};
+
+ReactDOM.render(
+   <App />,
+   document.getElementById('root'),
+);
